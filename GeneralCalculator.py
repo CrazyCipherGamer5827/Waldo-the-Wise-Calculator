5052a20114e578f3bd4be6f3f9ce5f6e33175947
20revsined <44682153+20revsined@users.noreply.github.com>
2019-04-06 11:50:51 -0400

    Update GeneralCalculator.py

--- GeneralCalculator.py
+++ GeneralCalculator.py
@@ -1,6 +1,6 @@
 import math
 
-class Basic Math():
+class BasicMath():
 	def __init__(self, add, subtract, multiply, divide):
 		self.add = add
 		self.subtract = subtract
@@ -8,21 +8,25 @@ class Basic Math():
 		self.divide = divide
 	
 	def add(number, i, Sum, iterations):
-		for i in iterations:
+		while i < iterations:
+			i = i + 1
 			Sum = Sum + number
 		return Sum
 
 	def subtract(number, i, difference, iterations):
-		for i in iterations:
+		while i < iterations:
+			i = i + 1
 			difference = difference - number
 		return differnce
 
 	def multiply(number, i, product, iterations):
-		for i in iterations:
+		while i < iterations:
+			i = i + 1
 			product = product * number
 		return product
 	def divide(number, i, quotient, iterations):
-		for i in iterations:
+		while i < iterations:
+			i = i + 1
 			quotient = quotient / number
 		return quotient
 
@@ -58,3 +62,15 @@ class TrigFunctions():
 	def cot(angle, value):
 		value = 1 / (math.tan(angle))
 		return value
+
+class Main():
+	question = str(input("Welcome to the general calculator! This calculator performs basic arithmatic operations as well as trigonometric functions! " +
+	"Please enter what part of the calculator you wish to use."))
+
+	if question == "basic":
+		global BasicMath
+		operation = str(input("What operation would you like to perform?"))
+
+		if operation == "add":
+			iterations = float(input("Please enter the number of iterations you wish to do."))
+			BasicMath = BasicMath.add(5, 6, 7, 8)
